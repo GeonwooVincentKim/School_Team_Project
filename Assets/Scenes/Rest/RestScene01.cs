5d90917788d539d83f371d4e098c3d5739f07a75
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-22 21:48:01 +0900

    Now we can go to Next Stage through RestScene

---
+++ Assets/Scenes/Rest/RestScene01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestScene01 : MonoBehaviour
+{
+    public Button Continue;
+    public Button Back;
+   
+    public void ChangeToSecondScene()
+    {
+        SceneManager.LoadScene("Scene4");
+    }
+
+    public void ChangetoIntroScene()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+}
