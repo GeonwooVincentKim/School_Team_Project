b67fa7e596f657247012408e45e04009a8696d6b
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-26 21:36:17 +0900

    Can Move RestScene01 to Scene4

---
+++ Assets/Scenes/Rest/RestScene02.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestScene02 : MonoBehaviour
+{
+    public Button Continue;
+    public Button Back;
+}
